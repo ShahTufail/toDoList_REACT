2fcf58a7df67a4c884fe82ba23c4b249bd4e6fae
Tufail Shah <shahtuffey@gmail.com>
2020-09-04 20:20:38 +0530

    build try

---
+++ build/precache-manifest.4e2d582fd663e4c417702a5dea112dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee503bd0d4d921a3f159169bfbf9f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35cafbcbe9c395fe9ac4",
+    "url": "/static/css/main.984a2400.chunk.css"
+  },
+  {
+    "revision": "c56d927697c141721035",
+    "url": "/static/js/2.14387dd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.14387dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cafbcbe9c395fe9ac4",
+    "url": "/static/js/main.5b53af36.chunk.js"
+  },
+  {
+    "revision": "9f08f0e23da8310728e7",
+    "url": "/static/js/runtime-main.5b849a39.js"
+  }
+]);
