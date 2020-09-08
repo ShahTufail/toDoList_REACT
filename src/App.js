681deee55ef380db6e6da415786b89b095310593
Tufail Shah <shahtuffey@gmail.com>
2020-09-08 20:05:14 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -91,3 +91,8 @@ class App extends Component {
   };
 }
 export default App;
+
+  // <img src={logo} className="App-logo" alt="logo" />
+  // <p>
+  //   Edit <code>src/App.js</code> and save to reload.
+  // </p>
